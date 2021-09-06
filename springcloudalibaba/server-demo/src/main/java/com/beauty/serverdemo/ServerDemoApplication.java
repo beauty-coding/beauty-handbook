799e47639eaad1deade2d35d95ec8588785e1391
yufw <yufw1066@qq.com>
2021-09-06 19:38:05 +0800

    [feat] alibaba spring cloud

---
+++ springcloudalibaba/server-demo/src/main/java/com/beauty/serverdemo/ServerDemoApplication.java
@@ -0,0 +1,15 @@
+package com.beauty.serverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class ServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerDemoApplication.class, args);
+    }
+
+}
---
+++ springcloudalibaba/src/test/java/com/beauty/springcloudalibaba/SpringcloudalibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beauty.springcloudalibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudalibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
