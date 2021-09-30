8f9a2b083c41fa33d472dec5450b2e82bf6bb876
yufw <yufw1066@qq.com>
2021-09-30 11:11:39 +0800

    [feat] 位操作、基础类型 大小

---
+++ java/src/main/java/com/beauty/base/BaseSize.java
@@ -0,0 +1,31 @@
+package com.beauty.base;
+
+/**
+ * description
+ *
+ * @author yufengwen
+ * @date 2021/9/29 11:22 上午
+ */
+public class BaseSize {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("==========bit大小============");
+        System.out.println("Character:" + Character.SIZE);
+        System.out.println("short:" + Short.SIZE);
+        System.out.println("int:" + Integer.SIZE);
+        System.out.println("long:" + Long.SIZE);
+        System.out.println("float:" + Float.SIZE);
+        System.out.println("double:" + Double.SIZE);
+
+        System.out.println("==========字节 byte 大小============");
+        System.out.println("Character:" + Character.BYTES);
+        System.out.println("short:" + Short.BYTES);
+        System.out.println("int:" + Integer.BYTES);
+        System.out.println("long:" + Long.BYTES);
+        System.out.println("float:" + Float.BYTES);
+        System.out.println("double:" + Double.BYTES);
+    }
+
+}
