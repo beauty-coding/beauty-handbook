d6f727c61acf957000e8d9c8e8322c9dc7c83322
yufengwen <yufengwen@baijia.com>
2021-12-16 20:14:10 +0800

    [feat] 设计模式 模板方法 完善，命令模式添加

--- design-patterns/src/main/java/com/beauty/designpatterns/behavior/TemplateMethodPattern.java
+++ design-patterns/src/main/java/com/beauty/designpatterns/behavior/TemplateMethodPattern.java
@@ -18,12 +18,59 @@ package com.beauty.designpatterns.behavior;
  * 父类中的抽象方法由子类实现，子类执行的结果会影响父类的结果，这导致一种反向的控制结构，它提高了代码阅读的难度。
  * 由于继承关系自身的缺点，如果父类添加新的抽象方法，则所有子类都要改一遍。
  *
+ * 模板方法模式包含以下主要角色。
+ * 1）抽象类/抽象模板（Abstract Class）
+ * 抽象模板类，负责给出一个算法的轮廓和骨架。它由一个模板方法和若干个基本方法构成。这些方法的定义如下。
+ *
+ * ① 模板方法：定义了算法的骨架，按某种顺序调用其包含的基本方法。
+ *
+ * ② 基本方法：是整个算法中的一个步骤，包含以下几种类型。
+ * 抽象方法：在抽象类中声明，由具体子类实现。
+ * 具体方法：在抽象类中已经实现，在具体子类中可以继承或重写它。
+ * 钩子方法：在抽象类中已经实现，包括用于判断的逻辑方法和需要子类重写的空方法两种。
+ * 2）具体子类/具体实现（Concrete Class）
+ * 具体实现类，实现抽象类中所定义的抽象方法和钩子方法，它们是一个顶级逻辑的一个组成步骤。
+ *
  * @author yufengwen
  * @date 2021/12/15 8:05 下午
  */
 public class TemplateMethodPattern {
 
+    public static void main(String[] args) {
+        AbstractBankBusinessTemplateMethod templateMethod = new ConcreteBody();
+        templateMethod.doWork();
+    }
+}
 
+//取号、排队、办理具体业务、对银行工作人员进行评分
+abstract class AbstractBankBusinessTemplateMethod{
 
+    private void getNo(){
+        System.out.println("取号");
+    }
+    private void lineUp(){
+        System.out.println("排队");
+    }
 
+    abstract void deal();
+
+    private void evaluate(){
+        System.out.println("评价");
+    }
+
+    public void doWork(){
+        getNo();
+        lineUp();
+        deal();
+        evaluate();
+    }
+
+}
+
+class ConcreteBody extends AbstractBankBusinessTemplateMethod{
+
+    @Override
+    public void deal() {
+        System.out.println("取款");
+    }
 }
